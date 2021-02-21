2a0e45af826258fad60dc325a948a0145fda02c8
yusupstudio <57658483+yusupsupriyadi@users.noreply.github.com>
2021-02-21 12:40:31 +0700

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="p-3">
                 <div className="flex items-center justify-center px-5 py-6 mx-auto text-base bg-blueGray-900 fle7-wrap md:ml-auto md:mr-auto">
                     <div className="flex flex-wrap items-center justify-center mx-auto text-base">
-                        <p className="mr-5 text-sm font-bold text-center text-gray-800 dark:-gray-100 dark:text-gray-200 lg:text-lg">©YusupSupriyadi-2020</p> <br/>
+                        <p className="mr-5 text-sm font-bold text-center text-gray-800 dark:-gray-100 dark:text-gray-200 lg:text-lg">©YusupSupriyadi-2021</p> <br/>
                         <a href="https://karyakarsa.com/Yusup%20Supriyadi" className="justify-center mr-5 font-bold text-sm text-center text-green-500 hover:text-blue-700 dark:text-green-500 lg:text-lg">Dukung!</a>
                         
                     </div>
